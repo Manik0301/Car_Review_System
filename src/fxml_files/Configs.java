018650cdcb76bf903615ce37352f3dfdadcd49ea
Manik0301 <bhagatmanik50@gmail.com>
2025-03-13 15:55:26 +0530

    Update Configs.java

--- src/fxml_files/Configs.java
+++ src/fxml_files/Configs.java
@@ -4,6 +4,6 @@ public class Configs {
     protected static String host = "@localhost";
     protected static String port = "1521";
     protected static String database = "xe";
-    protected static String dbUser = "manik";
-    protected static String dbPass = "2233643";
+    protected static String dbUser = "";
+    protected static String dbPass = "";
 }
